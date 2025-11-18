e08c7dcae82b447cb353f349def6f6b5f8b2582f
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2025-11-17 23:20:08 -0400

    primer commit

--- 001.py
+++ 001.py
@@ -1,1 +1,9 @@
+"""
+  Ejercicio 1
+  Sumar dos numeros y mostrar su resultado
+"""
+numero1 = 1
+numero2 = 2
+resultado = numero1 + numero2
 
+print("la suma es: ",resultado)
---
+++ 002.py
@@ -0,0 +1,5 @@
+"""
+  Ejercicio 2
+  Calcula el area de un ciruclo
+  con un radio dado
+"""
