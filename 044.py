0ddf6771faf30f8f9aa0ddc7b534425d0e825179
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2025-11-20 00:04:41 -0400

    44 correjido

--- 044.py
+++ 044.py
@@ -5,12 +5,12 @@ luego, pide al usuario adivinar el
 numero hasta que lo haga correctamente
 '''
 import random
-numero_secreto = random.random(1,10)
+numero_secreto = random.randint(1, 10)
 intentos = 0
 
 while True:
-  intento = int(input("Adivina el numwero"))
-  intentos = intento + 1
+  intento = int(input("Adivina el numwero, ingresa el numero por favor: "))
+  intentos = intentos + 1
   if intento  == numero_secreto:
     print(f"felicidades, adivinaste. solo te tomo {intentos} intentos lograrlo")
     break
